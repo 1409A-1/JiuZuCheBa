4f87c01bbb00aa14eb3682e40e7beb22135bcf89
Dong <dde-mail@qq.com>
2016-09-07 16:21:56 +0800

    修改资源目录

---
+++ resources/views/common/home_header.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <script src="home/js/b.js" charset="utf-8"></script>
+    <meta charset="UTF-8">
+    
+    <title>就租车吧—中国互联网租车领跑者,全国连锁.神速预订,48元起！</title>
+    <meta name="description" content="SEO：描述内容">
+    <meta name="keywords" content="SEO：站点关键字">
+    <link type="text/css" rel="stylesheet" href="home/css/all.css">
+    <link type="text/css" rel="stylesheet" href="home/css/index.css">
+    <link type="text/css" rel="stylesheet" href="home/css/calendar.css">
+    <script type="text/javascript" src="home/js/jquery-1.js"></script>
+    <script type="text/javascript" src="home/js/api"></script>
+    <script type="text/javascript" src="home/js/getscript"></script>
+    <script type="text/javascript" src="home/js/ApiConfig.js"></script>
+    <script type="text/javascript" src="home/js/layer.js"></script>
+    <link style="" id="" href="home/css/layer.css" rel="stylesheet">
+    <script type="text/javascript" src="home/js/jquery.js"></script>
+    <script type="text/javascript" src="home/js/dateRange.js"></script>
+    <script type="text/javascript" src="home/js/dateTools.js"></script>
+    <script type="text/javascript" src="home/js/all.js"></script>
+    <script type="text/javascript" src="home/js/index.js"></script>
+    <script>
+        var rh='';
+        function IsPC() {
+            var userAgentInfo = navigator.userAgent;
+            var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+            var flag = true;
+            for (var v = 0; v < Agents.length; v++) {
+                if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+            }
+            return flag;
+        }
+        if (rh != 'pc') {
+            if (!IsPC())
+                location.href = "http://m.dafang24.com";
+        }
+    </script>
+    <script src="home/js/bsl.js" charset="UTF-8" type="text/javascript"></script>
+    <script src="home/js/Enter.js" charset="UTF-8" id="BridgeRCVEnter" type="text/javascript"></script>
+    <script src="home/js/main_icon_invite_mess_api.js" charset="UTF-8" type="text/javascript"></script>
+    <link href="home/css/main.css" type="text/css" rel="stylesheet">
+    <link href="home/css/fix.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <!--头部-->
+<ins id="qiao-wrap">
+    <ins style="position: fixed; bottom: 0px;" class="qiao-flash-storage" id="qiao-flash-storage">
+        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" id="FlashLocalStorage" align="middle" height="5" width="5">
+            <param name="wmode" value="transparent"><param name="allowscriptaccess" value="always"><param name="movie" value="http://webim.qiao.baidu.com/f/pool/swf/local_storage.swf">
+            <embed wmode="transparent" allowscriptaccess="always" src="home/fonts/local_storage.swf" name="FlashLocalStorage" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" align="middle" height="5" width="5">
+        </object></ins><ins style="visibility: hidden; display: none;" class="qiao-invite-wrap" id="qiao-invite-wrap"><ins class="qiao-invite-decoration"></ins><ins style="visibility: hidden;" class="qiao-invite-text"><p style="font-size:12px;font-family:宋体;font-color:#000000;">欢迎您，有什么可以帮助您的么？</p>
+        </ins><ins class="qiao-invite-accept">现在咨询</ins><a class="qiao-invite-reject">稍后再说</a><a class="qiao-invite-close"></a><ins class="qiao-invite-form"><ins class="qiao-invite-form-inner"><textarea placeholder="您可直接在这里和我们联系！" class="qiao-invite-input"></textarea><a class="qiao-invite-send">发送</a></ins></ins></ins><ins style="visibility: hidden; display: none;" class="qiao-mess-wrap" id="qiao-mess-wrap"><ins class="qiao-mess-container" id="qiao-mess-container"><ins class="qiao-mess-head" id="qiao-mess-head"><ins id="qiao-mess-head-text" class="qiao-mess-head-text"></ins><a id="qiao-mess-head-close" data-status="max" class="qiao-mess-head-close"></a></ins><ins class="qiao-mess-body" id="qiao-mess-body"><iframe src="" style="display: none;" name="qiao-mess-iframe" id="qiao-mess-iframe"></iframe><form accept-charset="utf-8" action="http://qiao.baidu.com/v3/?module=default&amp;controller=index&amp;action=doMess&amp;siteid=6567291&amp;page_id=&amp;ucid=2941205" target="qiao-mess-iframe" method="post" class="qiao-mess-form qiao-mess-clearfix"><ins class="qiao-mess-clearfix"><ins class="qiao-mess-clearfix qiao-mess-item-mess"><ins style="display:none">留言内容</ins><textarea name="bd_bp_messText" class="" placeholder="请在此输入留言内容，我们会尽快与您联系。"></textarea><ins class="qiao-mess-star">*</ins></ins></ins><ins class="qiao-mess-item qiao-mess-clearfix"><ins class="qiao-mess-item-head">姓名<ins class="qiao-mess-star" style="display:none">*</ins></ins><ins class="qiao-mess-input-wrap"><input name="bd_bp_messName" class="qiao-mess-item-body" placeholder="最多100个字符"></ins></ins><ins class="qiao-mess-item qiao-mess-clearfix"><ins class="qiao-mess-item-head">电话<ins class="qiao-mess-star" style="display:none">*</ins></ins><ins class="qiao-mess-input-wrap"><input name="bd_bp_messPhone" class="qiao-mess-item-body" placeholder="请输入您的电话号码"></ins></ins><ins class="qiao-mess-item qiao-mess-clearfix"><ins class="qiao-mess-item-head">地址<ins class="qiao-mess-star" style="display:none">*</ins></ins><ins class="qiao-mess-input-wrap"><input name="bd_bp_messAddress" class="qiao-mess-item-body" placeholder="最多100个字符"></ins></ins><ins class="qiao-mess-item qiao-mess-clearfix"><ins class="qiao-mess-item-head">邮箱<ins class="qiao-mess-star" style="display:none">*</ins></ins><ins class="qiao-mess-input-wrap"><input name="bd_bp_messEmail" class="qiao-mess-item-body" placeholder="请输入合法邮箱名"></ins></ins><input style="display:none" value="大方租车—中国互联网租车领跑者,全国连锁.神速预订,48元起！" name="bd_bp_title" type="hidden"><input style="display:none" value="https://www.baidu.com/link?url=rBH8duhEszxR6WomeRnDLFrl4bnR8jhj-C2noO_xI2p_W5tZzyGs2rBgLdMEsVLj&amp;wd=&amp;eqid=e84d05c40006e7ee0000000257c61d1c" name="bd_bp_referer" type="hidden"><input style="display:none" value="1472601430611" name="bd_bp_tick" type="hidden"><input style="display:none" value="" name="bd_bp_bid" type="hidden"></form></ins><ins class="qiao-mess-foot" id="qiao-mess-foot"><a id="qiao-mess-foot-send-btn" class="qiao-mess-foot-send-btn">发送</a><ins class="qiao-mess-foot-logo"></ins></ins></ins></ins></ins><div class="top">
+        <a id="ibangkf" href="http://www.ibangkf.com">客服系统</a>
+        <script language="javascript" src="http://c.ibangkf.com/i/c-hhyhhy.js"></script>
+    <div class="top_box">
+        <a href="" rel="nofollow">中国互联网连锁租车品牌</a>
+        <ul class="top_menu">
+            <li class="top_user">
+                @if(empty(Session::get('user_name')))
+                <div class="no_users">
+                    <a href="login_reg" rel="nofollw">注册</a>
+                    <a href="login" rel="nofollw">登陆</a>
+                </div>
+                @else
+                <div class="yes_user">
+                    欢迎 <span style="color:#ff0000"><?php echo Session::get('user_name')?></span>
+                    <a href="" rel="nofollow">订单信息</a>
+                    <a href="{{URL('user_info')}}" rel="nofollow">账户管理</a>
+                    <a href="login_out" rel="nofollow">退出</a>
+                </div>
+                @endif
+            </li>
+            <li class="top_mobile">
+                <a href="" target="_blank" rel="nofollow">
+                    <i class="icon icon_phone"></i>
+                    手机预订
+                    <div class="QR_code">
+                        <div class="QR_code_caret"></div>
+                        <div class="QR_code_img"><img src="home/images/code.png"></div>
+                        <div class="QR_code_text">扫一扫，即刻体验</div>
+                    </div>
+                </a>
+            </li>
+            <li class="top_phone">400-060-0112</li>
+        </ul>
+    </div>
+</div>
+<!--菜单-->
+<div class="menu">
+    <div>
+        <i class="icon icon_logo"></i>
+        <ul class="menu_box">
+            <li class=" on_menu"><a href="/">首页</a></li>
+            <li class=""><a href="{{ url('driving') }}">短租自驾</a></li>
+            <li class=""><a href="lease_car">长租服务</a></li>
+            <li class=""><a href="e_rent_car">企业租车</a></li>
+            <li class=""><a href="" target="_blank">以租代购</a></li>
+            <li class=""><a href="">门店查询</a></li>
+            <li class=""><a href="">优惠活动</a></li>
+            <li class="" id="last_menu"><a href="" target="_blank">招商加盟</a></li>
+        </ul>
+    </div>
+</div>
