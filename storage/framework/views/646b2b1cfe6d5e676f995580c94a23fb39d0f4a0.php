d8a5709a14110a72bc1ad3ea32b75314e71de18b
zhaoag <525169093@qq.com>
2016-09-13 11:37:39 +0800

    9.13 add storage

---
+++ storage/framework/views/646b2b1cfe6d5e676f995580c94a23fb39d0f4a0.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Detail Admin - Tables showcase</title>
+    
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <!-- bootstrap -->
+    <link href="<?php echo e(asset('admin')); ?>/css/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="<?php echo e(asset('admin')); ?>/css/bootstrap/bootstrap-responsive.css" rel="stylesheet" />
+    <link href="<?php echo e(asset('admin')); ?>/css/bootstrap/bootstrap-overrides.css" type="text/css" rel="stylesheet" />
+
+    <!-- global styles -->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('admin')); ?>/css/layout.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('admin')); ?>/css/elements.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('admin')); ?>/css/icons.css" />
+
+    <!-- libraries -->
+    <link href="<?php echo e(asset('admin')); ?>/css/lib/font-awesome.css" type="text/css" rel="stylesheet" />
+    
+    <!-- this page specific styles -->
+    <link rel="stylesheet" href="<?php echo e(asset('admin')); ?>/css/compiled/tables.css" type="text/css" media="screen" />
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<body>
+
+    <!-- navbar -->
+    <?php echo $__env->make('common.admin_header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <!-- end navbar -->
+
+    <!-- sidebar -->
+    <?php echo $__env->make('common.admin_left', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <!-- end sidebar -->
+
+
+	<!-- main container -->
+    <div class="content">
+        
+        <!-- settings changer -->
+        <div class="skins-nav">
+            <a href="#" class="skin first_nav selected">
+                <span class="icon"></span><span class="text">Default</span>
+            </a>
+            <a href="#" class="skin second_nav" data-file="<?php echo e(asset('admin')); ?>/css/skins/dark.css">
+                <span class="icon"></span><span class="text">Dark skin</span>
+            </a>
+        </div>
+        
+        <div class="container-fluid">
+            <div id="pad-wrapper">
+                
+                <!-- products table-->
+                <!-- the script for the toggle all checkboxes from header is located in <?php echo e(asset('admin')); ?>/js/theme.js -->
+                <div class="table-wrapper products-table section">
+                    <div class="row-fluid head">
+                        <div class="span12">
+                            <h4>Products</h4>
+                        </div>
+                    </div>
+
+                    <div class="row-fluid filter-block">
+                        <div class="pull-right">
+                            <div class="ui-select">
+                                <select>
+                                  <option />Filter users
+                                  <option />Signed last 30 days
+                                  <option />Active users
+                                </select>
+                            </div>
+                            <input type="text" class="search" />
+                            <a class="btn-flat success new-product" id="ins">添加型号</a>
+                        </div>
+                    </div>
+                    <script src="<?php echo e(asset('admin')); ?>/js/js.js"></script>
+                    <script>
+                        $(function(){
+                            $("#ins").click(function(){
+                                location.href='model_add';
+                            });
+                        });
+                    </script>
+                    <div class="row-fluid">
+                        <table class="table">
+                            <tr>
+                                <th>服务点编号</th>
+                                <th>服务点名称</th>
+                                <th>所属地区</th>
+                                <th>操作</th>
+                            </tr>
+                            <tr>
+                                <?php foreach($data as $k => $v){?>
+                                <tr>
+                                    <td><?php echo e($v['server_id']); ?></td>
+                                    <td><?php echo e($v['server_name']); ?></td>
+                                    <td><?php echo e($v['address_name']); ?></td>
+                                    <td>
+                                        <a href="<?php echo e(url('addressDel')); ?>/<?php echo $v['server_id']?>">删除</a>
+                                    </td>
+                            </tr>
+                                <?php }?>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <!-- end products table -->
+                <!-- orders table -->
+        </div>
+    </div>
+    <!-- end main container -->
+
+	<!-- scripts -->
+    <script src="<?php echo e(asset('admin')); ?>/js/jquery-latest.js"></script>
+    <script src="<?php echo e(asset('admin')); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo e(asset('admin')); ?>/js/theme.js"></script>
+
+</body>
+</html>
